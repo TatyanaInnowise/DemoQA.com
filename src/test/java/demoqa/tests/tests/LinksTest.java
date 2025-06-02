31fe201851f9c96663abf6f8cf93145f2dba564f
Tatyana <tasya.go@gmail.com>
2025-06-02 13:11:17 +0300

    added Allure annotations

--- src/test/java/demoqa/tests/tests/LinksTest.java
+++ src/test/java/demoqa/tests/tests/LinksTest.java
@@ -1,12 +1,16 @@
 package demoqa.tests.tests;
 
-import demoqa.pages.LinksPage; // ✅ импортируй Page Object
-import demoqa.pages.WebTablePage;
+import demoqa.pages.LinksPage;
 import demoqa.tests.base.BaseTest;
+import io.qameta.allure.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+
+@Epic("DemoQA Links")
+@Feature("Link Navigation and Status Validation")
 public class LinksTest extends BaseTest {
 
     private LinksPage linksPage;
@@ -16,8 +20,12 @@ public class LinksTest extends BaseTest {
         driver.get("https://demoqa.com/links");
         linksPage = new LinksPage(driver); //
     }
-
     @Test
+    @Story("Verify broken link response")
+    @Severity(SeverityLevel.NORMAL)
+    @Description("Clicks on the 'Bad Request' link and checks that the status message contains the expected 400 response.")
+    @DisplayName("Check status after clicking 'Bad Request' link")
+
     public void testClickBadRequestLink() {
         LinksPage page = new LinksPage(driver);
         page.clickBadRequestLink();
@@ -28,8 +36,12 @@ public class LinksTest extends BaseTest {
                 statusText.contains("400 and status text Bad Request"),
                 "Expected: " + statusText);
     }
-
     @Test
+    @Story("Check redirection to Home page")
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("Clicks on the 'Home' link, switches to the new tab, and verifies the redirection URL.")
+    @DisplayName("Verify URL after clicking 'Home' link")
+
     public void compareUrls() {
         LinksPage page = new LinksPage(driver);
         page.clickHomeLink();
